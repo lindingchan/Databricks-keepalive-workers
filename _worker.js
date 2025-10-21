89c8dbfaa3d9543b182e8040c068e9e85d8d0161
lindingchan <73469714+lindingchan@users.noreply.github.com>
2025-10-21 13:36:57 +0800

    Update Databricks host and Telegram bot credentials

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 // 环境变量优先，没有则使用代码里填写的
 const DEFAULT_CONFIG = {
-  DATABRICKS_HOST: 'https://abc-1223456789.cloud.databricks.com',    // (必填)直接在单引号内填写工作区host或添加环境变量,变量名：DATABRICKS_HOST
-  DATABRICKS_TOKEN: 'dapi6dae4632d66931ecdeefe8808f12678dse',        // (必填)直接在单引号内填写token或添加环境变量,变量名：DATABRICKS_TOKEN
-  CHAT_ID: '',                                                       // 直接在单引号内填写Telegram聊天或添加环境变量CHAT_ID,须同时填写BOT_TOKEN(可选配置)
-  BOT_TOKEN: ''                                                      // 直接在单引号内填写Telegram机器人或添加环境变量,须同时填写CHAT_ID
+  DATABRICKS_HOST: 'https://dbc-a4f5d119-5683.cloud.databricks.com',    // (必填)直接在单引号内填写工作区host或添加环境变量,变量名：DATABRICKS_HOST
+  DATABRICKS_TOKEN: '',        // (必填)直接在单引号内填写token或添加环境变量,变量名：DATABRICKS_TOKEN
+  CHAT_ID: '1011577474',                                                       // 直接在单引号内填写Telegram聊天或添加环境变量CHAT_ID,须同时填写BOT_TOKEN(可选配置)
+  BOT_TOKEN: '8031697059:AAGN9sIRMBJxXugX4k9GhM9U9eU58C40PgY'                                                      // 直接在单引号内填写Telegram机器人或添加环境变量,须同时填写CHAT_ID
 };
 
 // 获取配置
